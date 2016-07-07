28de435cc82ec7a558dfd9a67ad0b4baec62d583
FireballGFX <marcel.zebrowski@gmx.de>
2016-07-07 20:59:21 +0200

    Test für die Overview hinzugefügt

--- Fuk/main.cpp
+++ Fuk/main.cpp
@@ -3,15 +3,33 @@
 
 #include "startlogo.h"
 
+#include "overview.h"
+#include "charaktermanager.h"
+#include "charakter.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    Startlogo s;
-    s.show();
+    //Startlogo s;
+    //s.show();
 
 
+    Ptr<CharakterManager> charakterManagerPtr = Ptr<CharakterManager>(new CharakterManager());
+
+    auto charakterManager = charakterManagerPtr.get();
+
+    charakterManager->addCharakterBeschreibung("Test","TEst");
+
+    auto charakter = charakterManager->getCurrentCharakter().lock();
+    QVector<Fertigkeit>* fertigkeiten = charakter->getFertigkeiten();
+    Fertigkeit fertigkeit = fertigkeiten->value(0);
+
+    //fertigkeit.setMerkmal(Merkmal::KLUGHEIT);
+    Overview o(NULL,charakterManagerPtr);
+    o.show();
 
 
+    //delete charakterManager;
     return a.exec();
 }
